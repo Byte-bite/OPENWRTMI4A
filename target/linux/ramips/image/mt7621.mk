c5f365a77ecc89b2a36942a704aedc87cf8f827d
Paul Fertser <fercerpav@gmail.com>
2019-10-25 13:18:43 +0300

    ramips: add support for Xiaomi Mi Wi-Fi Router 3G v2
    - CMIIT ID: 2019AP2581
    - SoC:      MediaTek MT7621
    - Flash:    16MiB NOR SPI (GigaDevice GD25Q128B)
    - RAM:      128MiB DDR3 (ESMT M15T1G1664A)
    - Serial:   As marked on PCB, 3V3 logic, baudrate is 115200, 8n1
    - Ethernet: 3x 10/100/1000 Mbps (switched, 2xLAN + WAN)
    - WIFI0:    MT7603E 2.4GHz 802.11b/g/n
    - WIFI1:    MT7612E 5GHz 802.11ac
    - Antennas: 4x external (2 per radio), non-detachable
    - LEDs:     Programmable "power" LED (two-coloured, yellow/blue)
                Non-programmable "internet" LED (shows WAN activity)
    - Buttons:  Reset
    
    INSTALLATION:
    
    Bootloader won't accept any serial input unless "boot_wait" u-boot
    environment variable is changed to "on". Vendor firmware (looks like
    an illegal OpenWrt fork) won't accept any serial input unless
    "uart_en" is set to "1". Tricks to force u-boot to use default
    environment do not help as it's restricted in the same way.
    
    With bootloader unlocked the easiest way would be to TFTP the
    sysupgrade image or to sysupgrade after loading an initramfs one.
    
    For porting the flash contents were changed externally with an SPI
    programmer (after lifting Vcc flash IC pin away from the PCB).
    
    Forum thread [0] indicates that this device is identical to "Xiaomi Mi
    Router 4A Gigabit Edition".
    
    [0] https://forum.openwrt.org/t/xiaomi-mi-router-4a-gigabit-edition-r4ag-r4a-gigabit-fully-supported-but-requires-overwriting-spi-flash-with-programmer/36685
    
    Signed-off-by: Paul Fertser <fercerpav@gmail.com>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -558,16 +558,38 @@ define Device/wf-2881
 endef
 TARGET_DEVICES += wf-2881
 
-define Device/mqmaker_witi-256m
-  DTS := WITI-256M
-  IMAGE_SIZE := $(ralink_default_fw_size_16M)
-  DEVICE_TITLE := MQmaker WiTi (256MB RAM)
+define Device/xiaomi_mir3g-v2
+  DTS := mt7621_xiaomi_mir3g-v2
+  MTK_SOC := mt7621
+  IMAGE_SIZE := 14848k
+  DEVICE_VENDOR := Xiaomi
+  DEVICE_TITLE := Xiaomi Mi Router 4A GIGABIT
+  DEVICE_VARIANT := v2
+  DEVICE_ALT0_VENDOR := Xiaomi
+  DEVICE_ALT0_MODEL := Mi Router 4A
+  DEVICE_ALT0_VARIANT := Gigabit Edition
+  DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 wpad-basic
+endef
+TARGET_DEVICES += xiaomi_mir3g-v2
+
+define Device/xiaomi_mir3p
+  MTK_SOC := mt7621
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_SIZE:= 4096k
+  UBINIZE_OPTS := -E 5
+  IMAGE_SIZE := 255488k
+  DEVICE_VENDOR := Xiaomi
+  DEVICE_MODEL := Mi Router 3 Pro
+  IMAGES += factory.bin
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi | check-size $$$$(IMAGE_SIZE)
   DEVICE_PACKAGES := \
 	kmod-ata-core kmod-ata-ahci kmod-mt76x2 kmod-sdhci-mt7620 kmod-usb3 \
 	kmod-usb-ledtrig-usbport wpad-basic
   SUPPORTED_DEVICES += witi
 endef
-TARGET_DEVICES += mqmaker_witi-256m
+TARGET_DEVICES += xiaomi_mir3g-v2
 
 define Device/mqmaker_witi-512m
   DTS := WITI-512M
